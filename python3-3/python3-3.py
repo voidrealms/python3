4346c3818dad87c557ec5af1ce031614f53b1313
Bryan Cairns <bcairns@voidrealms.com>
2020-10-29 14:43:06 -0400

    Add files via upload

---
+++ python3-3/python3-3.py
@@ -0,0 +1,31 @@
+#This is a comment
+#This is our 3rd video!!!
+
+"""
+This is a comment
+written in
+more than just one line
+More actually this is a string and 
+using this might cause issues later
+"""
+
+#x = "Hello World"
+#print(x)
+
+#Booleans True or False (like a light switch)
+x = True #Notice this is case sensitive
+y = False #Notice this is case sensitive
+
+#Comparisons - the building block of logic
+print(f'x = {x}')
+print(f'y = {y}')
+
+print(f'Equal: {x == y}')
+print(f'Not Equal: {x != y}')
+
+print(f'Greater than: {x > y}')
+print(f'Greater than or equal: {x >= y}')
+
+print(f'Less than: {x < y}')
+print(f'Less than or equal: {x < y}')
+
