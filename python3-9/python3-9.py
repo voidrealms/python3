dcaa17f24ca863fc70d8ae64b9c7d8c9a94cd8d1
Bryan Cairns <bcairns@voidrealms.com>
2020-11-05 12:16:28 -0500

    Add files via upload

---
+++ python3-9/python3-9.py
@@ -0,0 +1,25 @@
+#Tuple ()
+#Tuple is a fast list that can not be modified
+
+#Create a tuple
+t = (1,2,3,4)
+print(t)
+
+#Access elements
+print(f'Index: {t[0]}')
+print(f'Slice: {t[2:]}')
+print(f'Bool: {3 in t}')
+
+#Assignment
+(x,y,z) = (1,2,3)
+print(x)
+print(y)
+print(z)
+
+(x,y,z) = range(1,4)
+print(x)
+print(y)
+print(z)
+
+
+
